a01feb0dd7cc061feaee8775cbdc0243fb2272a1
Thai Bao <buithaibao0309@gmail.com>
2024-05-18 23:39:12 +0700

    Fix debug stuff

--- debug.hpp
+++ debug.hpp
@@ -1,57 +1,54 @@
-#pragma one
-
+#pragma once
 #include <bits/stdc++.h>
 
-using namespace std;
-
-void __print(short x) { cerr << x; }
-void __print(int x) { cerr << x; }
-void __print(long x) { cerr << x; }
-void __print(long long x) { cerr << x; }
-void __print(unsigned x) { cerr << x; }
-void __print(unsigned long x) { cerr << x; }
-void __print(unsigned long long x) { cerr << x; }
-void __print(float x) { cerr << x; }
-void __print(double x) { cerr << x; }
-void __print(long double x) { cerr << x; }
-void __print(char x) { cerr << '\'' << x << '\''; }
-void __print(const char *x) { cerr << '\"' << x << '\"'; }
-void __print(const string &x) { cerr << '\"' << x << '\"'; }
-void __print(bool x) { cerr << (x ? "true" : "false"); }
+void __print(short x) { std::cerr << x; }
+void __print(int x) { std::cerr << x; }
+void __print(long x) { std::cerr << x; }
+void __print(long long x) { std::cerr << x; }
+void __print(unsigned x) { std::cerr << x; }
+void __print(unsigned long x) { std::cerr << x; }
+void __print(unsigned long long x) { std::cerr << x; }
+void __print(float x) { std::cerr << x; }
+void __print(double x) { std::cerr << x; }
+void __print(long double x) { std::cerr << x; }
+void __print(char x) { std::cerr << '\'' << x << '\''; }
+void __print(const char *x) { std::cerr << '\"' << x << '\"'; }
+void __print(const std::string &x) { std::cerr << '\"' << x << '\"'; }
+void __print(bool x) { std::cerr << (x ? "true" : "false"); }
 template <typename A> void __print(const A &x);
-template <typename A, typename B> void __print(const pair<A, B> &p);
-template <typename... A> void __print(const tuple<A...> &t);
-template <typename T> void __print(stack<T> s);
-template <typename T> void __print(queue<T> q);
-template <typename T, typename... U> void __print(priority_queue<T, U...> q);
+template <typename A, typename B> void __print(const std::pair<A, B> &p);
+template <typename... A> void __print(const std::tuple<A...> &t);
+template <typename T> void __print(std::stack<T> s);
+template <typename T> void __print(std::queue<T> q);
+template <typename T, typename... U> void __print(std::priority_queue<T, U...> q);
 template <typename A> void __print(const A &x) {
 	bool first = true;
-	cerr << '{';
+	std::cerr << '{';
 	for (const auto &i : x) {
-		cerr << (first ? "" : ","), __print(i);
+		std::cerr << (first ? "" : ","), __print(i);
 		first = false;
 	}
-	cerr << '}';
+	std::cerr << '}';
 }
-template <typename A, typename B> void __print(const pair<A, B> &p) {
-	cerr << '(';
+template <typename A, typename B> void __print(const std::pair<A, B> &p) {
+	std::cerr << '(';
 	__print(p.first);
-	cerr << ',';
+	std::cerr << ',';
 	__print(p.second);
-	cerr << ')';
+	std::cerr << ')';
 }
-template <typename T> void __print(stack<T> s) {
-	vector<T> debugVector;
+template <typename T> void __print(std::stack<T> s) {
+	std::vector<T> debugVector;
 	while (!s.empty()) {
 		T t = s.top();
 		debugVector.push_back(t);
 		s.pop();
 	}
-	reverse(debugVector.begin(), debugVector.end());
+	std::reverse(debugVector.begin(), debugVector.end());
 	__print(debugVector);
 }
-template <typename T> void __print(queue<T> q) {
-	vector<T> debugVector;
+template <typename T> void __print(std::queue<T> q) {
+	std::vector<T> debugVector;
 	while (!q.empty()) {
 		T t = q.front();
 		debugVector.push_back(t);
@@ -59,8 +56,8 @@ template <typename T> void __print(queue<T> q) {
 	}
 	__print(debugVector);
 }
-template <typename T, typename... U> void __print(priority_queue<T, U...> q) {
-	vector<T> debugVector;
+template <typename T, typename... U> void __print(std::priority_queue<T, U...> q) {
+	std::vector<T> debugVector;
 	while (!q.empty()) {
 		T t = q.top();
 		debugVector.push_back(t);
@@ -68,19 +65,16 @@ template <typename T, typename... U> void __print(priority_queue<T, U...> q) {
 	}
 	__print(debugVector);
 }
-void _print() { cerr << "]\n"; }
+void _print() { std::cerr << "]\n"; }
 template <typename Head, typename... Tail>
 void _print(const Head &H, const Tail &...T) {
 	__print(H);
-	if (sizeof...(T))
-		cerr << ", ";
+	if (sizeof...(T)) std::cerr << ", ";
 	_print(T...);
 }
 
 #ifndef DEBUG
-#define debug(...)                                                             \
-	cerr << "Line " << __LINE__ << ": [" << #__VA_ARGS__ << "] = [";       \
-	_print(__VA_ARGS__)
+#define debug(...) std::cerr << "Line " << __LINE__ << ": [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
 #else
 #define debug(...) 2004
 #endif
@@ -89,9 +83,6 @@ template <typename F> void benchmark(const std::string &func_name, const F &f) {
 	auto start = std::chrono::high_resolution_clock::now();
 	f();
 	auto end = std::chrono::high_resolution_clock::now();
-	double time_taken =
-	    std::chrono::duration_cast<chrono::nanoseconds>(end - start)
-		.count();
-	std::cerr << "Benchmark " << func_name << " : " << time_taken << " ns\n"
-		  << flush;
+	double time_taken = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	std::cerr << "Benchmark " << func_name << " : " << time_taken << " ns\n" << std::flush;
 }
