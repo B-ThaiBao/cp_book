94d2a7d725c52b7d7983bf43511b897cec2a0763
Thai Bao <buithaibao0309@gmail.com>
2024-06-03 20:43:59 +0700

    Change compressor into std::vector

--- misc/compressor.hpp
+++ misc/compressor.hpp
@@ -1,48 +1,21 @@
 /**
  * COMPRESSOR:
  * Provide machine to compress the number fix into the array (number can be smallest as possible)
- * 
+ *
  * Example: Compress: {1, 3, 6, 3} -> {0, 1, 2, 1}
  * For compress an array, you just use compress (it may be faster than this compressor)
 **/
-template <typename T> struct compressor {
-	bool already_compress = false;
-	std::vector<T> data;
+template <typename T> struct compressor : public std::vector<T> {
+	using std::vector<T>::vector;
 
-	compressor(){}
-	compressor(const int& N){
-		// Beware of this one, may be it can be relocation if number of elements > N
-		data.reserve(N); // Avoid relocation
+	inline void compress() {
+		std::sort(this->begin(), this->end());
+		this->erase(std::unique(this->begin(), this->end()), this->end());
 	}
-
-	inline void compress(){
-		std::sort(data.begin(), data.end());
-		data.erase(std::unique(data.begin(), data.end()), data.end());
-		already_compress = true;
+	inline int operator()(const T &x) const {
+		return int(std::lower_bound(this->begin(), this->end(), x) - this->begin());
 	}
-	inline void insert(const T& x) { data.emplace_back(x); }
-
-	inline int operator () (const T& x) {
-		if (!already_compress) compress();
-		return std::lower_bound(data.begin(), data.end(), x) - data.begin();
-	}
-	inline T& operator [] (const int& x) {
-		if (!already_compress) compress();
-		return data[x];
-	}
-	inline T& find_value(const int& x) {
-		if (!already_compress) compress();
-		return data[x];
-	}
-
-	inline int size() {
-		if (!already_compress) compress();
-		return static_cast<int>(data.size());
-	}
-	inline int max_element(){
-		return size() - 1;
-	}
-}; // compressor
+};
 
 // NOTE: Using when you want to compress the numbers but don't want to change their order in O(nlogn)
 // Source: https://codeforces.com/blog/entry/84164?#comment-716682
