014180cd83208e052420a350b5ad4c87bf14939f
Thai Bao <buithaibao0309@gmail.com>
2026-02-02 13:45:04 +0700

    [order_statistic] Fix documentation

--- ds/order_statistic.hpp
+++ ds/order_statistic.hpp
@@ -9,7 +9,7 @@ using order_statistic_map = __gnu_pbds::tree<
 template <typename K, typename Comp = std::less<K>>
 using order_statistic_set = order_statistic_map<K, __gnu_pbds::null_type, Comp>;
 
-//  Keep pair if index and value for multiset usage
 // Supports:
 //  auto iterator = order_statistic_set().find_by_order(idx); // (0-indexed)
 //  int num_strictly_smaller = order_statistic_set().order_of_key(key);
+// TODO: Keep pair of index and value for multiset usage
